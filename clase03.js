bb2596e72ccb0da510638a2ff7403448cb23d414
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-03-03 12:27:39 +0100

    Fin de la sesion L06

--- clase03.js
+++ clase03.js
@@ -15,4 +15,59 @@ Se presenta la tabla azul donde se tratan las formas habituales de gestionar los
 Formato de datos, serialización y deserializacion, convertir objetos en cadenas y el camino contrario.
 Habla sobre YAML
 
-Relaciones sobre recursos (datos complejos) */
+Relaciones sobre recursos (datos complejos)
+
+Creacion de recursos*/
+
+var express = require("express");
+var cool = require("cool-ascii-faces");
+
+
+var app = express();
+var port = process.env.PORT || 12345;
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+
+var contacts = [
+    {
+        name: 'pepe',
+        phone: 1234
+    },
+    {
+        name: 'pablo',
+        phone: 6789
+    }
+];
+
+const BASE_API_URL = `/api/v1`;
+
+app.get(BASE_API_URL+"/contacts", (request,response) => {
+    response.json(contacts);
+    console.log("New request to /contacts");
+});
+
+app.post(BASE_API_URL+"/contacts", (request,response) => {
+
+    var newContact = request.body;
+
+
+    console.log(`newContact = ${JSON.stringify(newContact, null, 2)}`);
+   
+    console.log("New POST to /contacts");
+
+    contacts.push(newContact);
+
+    response.sendStatus(201);
+});
+
+app.get("/faces", (request,response) => {
+    response.send(cool());
+    console.log("New request");
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server ready in port ${port}`);
+});
+
