ea50812a892f4cb8c7a901186f0fe25a5f2b6e19
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-03-10 11:45:46 +0100

    Despliegue en google cloud

---
+++ backend/index.js
@@ -0,0 +1,70 @@
+const BASE_API_URL = `/api/v1`;
+var Datastore = require('nedb');
+var db = new Datastore();
+
+
+module.exports = (app) => {
+    
+    var contacts = [
+        {
+            name: 'pepe',
+            phone: 1234
+        },
+        {
+            name: 'pablo',
+            phone: 6789
+        }
+    ];
+    
+    db.insert(contacts);
+    console.log('Inserted 2 contacts on start.');
+    
+    app.get(BASE_API_URL+"/contacts", (request,response) => {
+        console.log("New request to /contacts");
+        db.find({}, (err, contacts) => {
+            if(err){
+                console.log(`Error getting /contacts: ${err}`);
+                response.sendStatus(500);
+            }else{
+                console.log(`Contacts returned ${contacts.length}`);
+                response.json(contacts.map((c) => {
+                    delete c._id
+                    return(c);
+                }));
+            }
+        })
+       
+    });
+    
+    app.post(BASE_API_URL+"/contacts", (request,response) => {
+    
+        var newContact = request.body;
+    
+    
+        console.log(`newContact = ${JSON.stringify(newContact, null, 2)}`);
+       
+        console.log("New POST to /contacts");
+        
+        db.insert(newContact);
+    
+        response.sendStatus(201);
+    });
+
+
+    app.delete(BASE_API_URL+'/contacts/:name', (request, response) => {
+        var name = request.params.name;
+
+        console.log(`New delete to /contacts/${name}`)
+
+        db.delete({'name': name}, {},  (err, numRemoved) => {
+            if(err){
+                console.log(`Error deletting /contacts/${name}: ${err}`);
+                response.sendStatus(500);
+            }else{
+                console.log(`Contacts removed ${numRemoved}`);
+                response.sendStatus(200);
+            }
+        })
+    });
+    
+};
--- clase04.js
+++ clase04.js
@@ -1,47 +1,21 @@
 /* Backend REST II:
 
 */
+
+//_____________________________REQUIRES___________________________________
+
 var express = require("express");
 var bodyParser = require('body-parser');
+var backend = require('./backend');
 
 var app = express();
 var port = process.env.PORT || 12345;
 
 app.use("/", express.static("./public"));
-
+    
 app.use(bodyParser.json());
 
-var contacts = [
-    {
-        name: 'pepe',
-        phone: 1234
-    },
-    {
-        name: 'pablo',
-        phone: 6789
-    }
-];
-
-const BASE_API_URL = `/api/v1`;
-
-app.get(BASE_API_URL+"/contacts", (request,response) => {
-    response.json(contacts);
-    console.log("New request to /contacts");
-});
-
-app.post(BASE_API_URL+"/contacts", (request,response) => {
-
-    var newContact = request.body;
-
-
-    console.log(`newContact = ${JSON.stringify(newContact, null, 2)}`);
-   
-    console.log("New POST to /contacts");
-
-    contacts.push(newContact);
-
-    response.sendStatus(201);
-});
+backend(app);
 
 app.get("/faces", (request,response) => {
     response.send(cool());
